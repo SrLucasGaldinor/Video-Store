339e8ce45859e11e168702ea48f30d28ea5f2e4d
SrLucasGaldinor <lucasgaldinorosa2904@gmail.com>
2025-12-27 10:54:44 -0300

    First Commit

---
+++ src/main/java/com/LGR/video_store/GaldinoVideoApplication.java
@@ -0,0 +1,13 @@
+package com.LGR.video_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaldinoVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaldinoVideoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LGR/video_store/GaldinoVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LGR.video_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaldinoVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
