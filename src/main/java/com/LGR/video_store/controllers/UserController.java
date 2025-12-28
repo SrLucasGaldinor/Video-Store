965322513397646e7d1dee35b3d2526a16d81cb8
SrLucasGaldinor <lucasgaldinorosa2904@gmail.com>
2025-12-28 20:00:21 -0300

    refactor: adapt UserController to use DTO-based API contracts

--- src/main/java/com/LGR/video_store/controllers/UserController.java
+++ src/main/java/com/LGR/video_store/controllers/UserController.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.LGR.video_store.entities.User;
+import com.LGR.video_store.dtos.UserCreateDTO;
+import com.LGR.video_store.dtos.UserResponseDTO;
+import com.LGR.video_store.dtos.UserUpdateDTO;
 import com.LGR.video_store.services.UserService;
 
 import jakarta.validation.Valid;
@@ -18,21 +23,37 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-	
+
 	private final UserService service;
-	
+
 	public UserController(UserService service) {
 		this.service = service;
 	}
-
+	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserResponseDTO>> findAll() {
 		return ResponseEntity.ok(service.findAll());
 	}
 	
+	@GetMapping("{id}")
+	public ResponseEntity<UserResponseDTO> findById(@PathVariable Long id) {
+		return ResponseEntity.ok(service.findById(id));
+	}
+
 	@PostMapping
-	public ResponseEntity<User> create(@Valid @RequestBody User user) {		
-			return ResponseEntity.status(HttpStatus.CREATED).body(service.create(user));			
+	public ResponseEntity<UserResponseDTO> create(@Valid @RequestBody UserCreateDTO dto) {
+		UserResponseDTO createdUser = service.create(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+	}
+
+	@PutMapping("{id}")
+	public ResponseEntity<UserResponseDTO> update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto) {
+		return ResponseEntity.ok(service.update(id, dto));
 	}
 	
+	@DeleteMapping("{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
