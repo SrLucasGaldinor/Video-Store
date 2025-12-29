e81a6b6c19607384508b74f56cf784ac2accd616
SrLucasGaldinor <lucasgaldinorosa2904@gmail.com>
2025-12-29 16:49:10 -0300

    feat: standardize API error handling with global exception handler
    - Added ErrorResponse to define a consistent API error format
    - Added simple validation error handling for @Valid requests

--- src/main/java/com/LGR/video_store/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/LGR/video_store/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,47 @@
 package com.LGR.video_store.exceptions;
 
+import java.time.Instant;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage()); 
+	public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex,
+															    HttpServletRequest request) {
+		ErrorResponse error = new ErrorResponse(Instant.now(),
+												HttpStatus.NOT_FOUND.value(),
+												"Resource not found!",
+												ex.getMessage(),
+												request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error); 
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex,
+																 HttpServletRequest request) {
+		
+		String message = ex.getBindingResult()
+						   .getFieldErrors()
+						   .get(0)
+						   .getDefaultMessage();
+				
+		ErrorResponse error = new ErrorResponse(Instant.now(),
+												HttpStatus.BAD_REQUEST.value(),
+												"Validation error!",
+												message,
+												request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
 }
